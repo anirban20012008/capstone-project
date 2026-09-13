bef7357ff14ea207b86b8c48c8de36eec01d2c01
Anirban <karsathi67@gmail.com>
2026-09-13 13:45:24 +0530

    Add verify.sql

--- verify.sql
+++ verify.sql
@@ -1,7 +1,5 @@
-SELECT 'products' AS table_name, COUNT(*) FROM products
-UNION ALL
-SELECT 'customers', COUNT(*) FROM customers
-UNION ALL
-SELECT 'orders', COUNT(*) FROM orders
-UNION ALL
-SELECT 'category_targets', COUNT(*) FROM category_targets;
+SELECT COUNT(*) FROM products ;
+SELECT COUNT(*) FROM customers ;
+SELECT COUNT(*) FROM orders;
+SELECT COUNT(*) FROM category_targets ;
+SELECT status , COUNT(*) FROM orders GROUP BY status;
