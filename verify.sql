49e8a4eaba2c3ce95f371f80d6b47c2deb4e91d4
Anirban <karsathi67@gmail.com>
2026-09-13 13:54:52 +0530

    Add verify.sql

--- verify.sql
+++ verify.sql
@@ -1,5 +1,12 @@
+-- VERIFICATION RESULTS 
+-- products : 31
+-- customers : 50
+-- orders : 500
+-- category_targets : 6
+-- orders status : Delivered = 434 ,Cancelled = 42 , Pending = 24
+
 SELECT COUNT(*) FROM products ;
 SELECT COUNT(*) FROM customers ;
 SELECT COUNT(*) FROM orders;
 SELECT COUNT(*) FROM category_targets ;
-SELECT status , COUNT(*) FROM orders GROUP BY status;
+SELECT status , COUNT(*) FROM orders GROUP BY status  ;
